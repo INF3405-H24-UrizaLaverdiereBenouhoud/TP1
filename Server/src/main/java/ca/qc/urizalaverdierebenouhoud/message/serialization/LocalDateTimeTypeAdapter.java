c75164743c923fda7c53fff2757174a7db6107be
Mehdi Benouhoud <42016376+mehdiben7@users.noreply.github.com>
2024-01-16 22:01:28 -0500

    Sauvegarder les messages (#15)
    * Clean time format + loadMessages + return last 15 messages or less
    
    * cleaner code

---
+++ Server/src/main/java/ca/qc/urizalaverdierebenouhoud/message/serialization/LocalDateTimeTypeAdapter.java
@@ -0,0 +1,22 @@
+package ca.qc.urizalaverdierebenouhoud.message.serialization;
+
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeTypeAdapter implements JsonSerializer<LocalDateTime>, JsonDeserializer<LocalDateTime> {
+    private static final DateTimeFormatter LOAD_DATE_TIME_FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+    public static final DateTimeFormatter DISPLAY_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd@HH:mm:ss");
+
+    @Override
+    public JsonElement serialize(LocalDateTime dateTime, Type type, JsonSerializationContext jsonSerializationContext) {
+        return new JsonPrimitive(dateTime.format(LOAD_DATE_TIME_FORMATTER));
+    }
+
+    @Override
+    public LocalDateTime deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+        return LocalDateTime.parse(jsonElement.getAsString(), LOAD_DATE_TIME_FORMATTER);
+    }
+}
