e0a1643d8dde69a1c329eae0c32c6981da173f97
Kujaku223 <112909425+Kujaku223@users.noreply.github.com>
2024-01-27 13:59:48 -0500

    12 faire sockets du côté serveur et client (#24)
    * sever sockets
    
    * initial server socket
    
    * added message buffer, initialisation
    
    * added comment
    
    * made some variale attributes of Server
    
    * started working on client
    
    * client works
    
    * can send multiple messages from client
    
    * server cleanup
    
    * added various login options
    
    * task encoding works
    
    * Initial Socket connection
    
    * created Main client
    
    * cleaner main client
    
    * more cleanup
    
    * cleanup
    
    * swithed the way ip is handled for com
    
    * user can disconnect and relog without server crashing
    
    * tried multithread
    
    * client handler works
    
    * REEEEEEEEEEEEEEEE
    
    * ree
    
    * cleanup
    
    * cleanup
    
    * fixed crash
    
    ---------
    
    Co-authored-by: Mehdi Benouhoud <42016376+mehdiben7@users.noreply.github.com>

--- Server/src/main/java/ca/qc/urizalaverdierebenouhoud/Server.java
+++ Server/src/main/java/ca/qc/urizalaverdierebenouhoud/Server.java
@@ -1,83 +1,42 @@
 package ca.qc.urizalaverdierebenouhoud;
-
+import ca.qc.urizalaverdierebenouhoud.server.ClientHandler;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-    public class Server {
-        private static ServerSocket server;
+public class Server {
 
-        private static String serverAddress ="0.0.0.0";  //TODO: allow user to change IP at connection
-        private static int serverPort = 5003;
-        public static void main(String[] args) throws Exception {
-          server = new ServerSocket(); // initialize server
+    public static void main(String[] args) throws Exception {
+        ServerSocket server = new ServerSocket(); // initialize server
+        //server put on listen
+        try (server) {
             startServer(server);
-            //serveur mis en écoute sur ce point
-            try {
-                while (true) {
-                    Socket client = server.accept(); //blocs code until connection request is made
-
-                    //    Should create switch case with with Byte readByte(); of DataInputStream
-                    DataInput message = new DataInputStream(client.getInputStream());
-                    interpretStreamContent(message);
-
-                   //should send confirmation message is received
-
-                }
-            } finally {
-                server.close();
+            int number = 0;
+            while (!server.isClosed()) {
+                Socket client = server.accept(); //blocs code until connection request is made
+                ClientHandler handler = new ClientHandler(client, number++);
+                handler.start();
+                //should send confirmation message is received
             }
-        }
-        private static void interpretStreamContent(DataInput in) throws IOException
-        {
-            switch ( (int)readFirstByte(in))
-            {
-                case 0: //login
-
-                    break;
-
-                case 1: // send recent history
-                    break;
-
-                case 2: // client sent message
-                    System.out.println("task 2 initiated"); //TODO: enlever avant remise
-                    // Stays here for debugging pupopose prcq le serveur fonctionne pour 1 personne
-                    // mais pas encore avec plusieur clients
-                    readMessage(in);
-                    break;
-                default:
-                    System.out.println("No task associated with Byte");
-            }
-        }
-
-        private static Byte readFirstByte(DataInput in)
-        {
-            try {
-                Byte task = in.readByte();
-                System.out.println("task type: "+ task);
-                return task;
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        private static void readMessage(DataInput message) throws IOException  // not sure if this is right
-        {
-            System.out.println(message.readUTF());
-        }
-
-        private static void startServer(ServerSocket server)
-        {
-             try{
-                 server.setReuseAddress(true); // so socket does not enter timewait state
-                 InetAddress serverIP = InetAddress.getByName(serverAddress);
-                 server.bind(new InetSocketAddress(serverIP, serverPort)); //define communication endpoint (point d'entré)
-                 System.out.format("The server is running on %s:%d %n", serverAddress, serverPort);
-             }
-            catch (IOException e){
-                throw new RuntimeException(e);
-            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
+
+    private static void startServer(ServerSocket server) {
+        try {
+            server.setReuseAddress(true); // so socket does not enter timewait state
+            //TODO: allow user to change IP at connection
+            String serverAddress = "0.0.0.0";
+            int serverPort = 5003;
+            InetAddress serverIP = InetAddress.getByName(serverAddress);
+            server.bind(new InetSocketAddress(serverIP, serverPort)); //define communication endpoint (point d'entré)
+            System.out.format("The server is running on %s:%d %n", serverAddress, serverPort);
+        } catch (IOException e) {
+            //throw new RuntimeException(e);
+        }
+    }
+}
+
