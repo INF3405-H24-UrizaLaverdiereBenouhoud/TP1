132bad6baa565ee30e631fedd47ce7ab5ade9299
Kujaku223 <112909425+Kujaku223@users.noreply.github.com>
2024-02-06 17:53:41 -0500

    12 faire sockets du côté serveur et client (#28)
    * sever sockets
    
    * initial server socket
    
    * added message buffer, initialisation
    
    * added comment
    
    * made some variale attributes of Server
    
    * started working on client
    
    * client works
    
    * can send multiple messages from client
    
    * server cleanup
    
    * added various login options
    
    * task encoding works
    
    * Initial Socket connection
    
    * created Main client
    
    * cleaner main client
    
    * more cleanup
    
    * cleanup
    
    * swithed the way ip is handled for com
    
    * user can disconnect and relog without server crashing
    
    * tried multithread
    
    * client handler works
    
    * REEEEEEEEEEEEEEEE
    
    * ree
    
    * cleanup
    
    * cleanup
    
    * fixed crash
    
    * added static handler list
    
    * return
    
    * message sent to server display with username
    
    * message properly formatted
    
    * ppushing so I can switch branch
    
    * can exit properly
    
    * cleanup and clean exit from client
    
    ---------
    
    Co-authored-by: Mehdi Benouhoud <42016376+mehdiben7@users.noreply.github.com>

--- Server/src/main/java/ca/qc/urizalaverdierebenouhoud/users/MainClient.java
+++ Server/src/main/java/ca/qc/urizalaverdierebenouhoud/users/MainClient.java
@@ -1,16 +1,18 @@
 package ca.qc.urizalaverdierebenouhoud.users;
+
 import java.io.*;
 import java.net.Inet4Address;
 import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class MainClient {
-   // TestipAddress = "192.168.100.133";
+    // TestipAddress = "192.168.100.133";
     private static final int TestPort = 5003;
 
     private static Client baseClient;
-
-
+    private static boolean isRunning = true;
 
     public static void main(String[] args) {
         try {
@@ -26,7 +28,7 @@ public class MainClient {
             //validation
             Inet4Address address = (Inet4Address) Inet4Address.getByName(ip);
             Account account = new Account("dummy account", "dummy");
-            baseClient = new Client(account,address, TestPort);
+            baseClient = new Client(account, address, TestPort);
             //if user does not exist add to DB
 
             //Display historic
@@ -34,12 +36,11 @@ public class MainClient {
             //send message TODO: need to implement return from server
             chatRoomFunctionalities(scanner);
 
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             //throw new RuntimeException(e);
         }
     }
+
     ////////////////////////////////////////////////////////
     //Milestone in connection
     ////////////////////////////////////////////////////////
@@ -47,28 +48,38 @@ public class MainClient {
         try (Socket client = new Socket(baseClient.getIpAddress(), baseClient.getPort())) {
             DataOutputStream out = new DataOutputStream(client.getOutputStream());
             listen(client);
-            while (client.isConnected()) {
+            while (isRunning) {
                 String message = scanner.nextLine();
+                if(message.equals("exit")) {
+                    isRunning = false;
+                    out.writeByte(4);
+                    out.flush();
+                    client.close();
+                }
+                else {
+                    if((!message.isEmpty())&&message.length()<=200)
                 sendMessageToChat(out, message);
-            }
+            }}
             //client.close
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            isRunning = false;
         }
     }
+
     private static void listen(Socket client) throws IOException {
         new Thread(new Runnable() {
             @Override
             public void run() {
                 BufferedReader in;
                 try {
-                    while (true) {
+                    while (isRunning) {
                         in = new BufferedReader(new InputStreamReader(client.getInputStream()));
                         String message = in.readLine();
-                        System.out.println(message);
+                        if(message != null)
+                           System.out.println(message);
                     }
                 } catch (IOException e) {
-                    throw new RuntimeException(e);
+                   isRunning = false;
                 }
             }
         }).start();
@@ -79,28 +90,33 @@ public class MainClient {
     ////////////////////////////////////////////////////////
     // Task methods in Main
     /////////////////////////////////////
-    private static void sendMessageToChat(DataOutputStream out, String message) {
+    private static void sendMessageToChat(DataOutputStream out, String message) throws IOException {
         encodeAndSend(2, out, message);
     }
 
     /////////////////////////////////////
     // called methods for sending
     /////////////////////////////////////
-    private static void encodeAndSend(int task, DataOutputStream out, String message) {
-        try {
+    private static void encodeAndSend(int task, DataOutputStream out, String message) throws IOException {
             out.writeByte(task);
             sendMessage(out, message);
-        } catch (IOException e) {
-            //throw new RuntimeException(e);
-        }
     }
 
     private static void sendMessage(DataOutputStream out, String message) {
         try {
-            out.writeUTF(message);
+            if (message.isEmpty())
+                return;
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy '@' HH:mm:ss");
+            String formattedDate = now.format(formatter);
+            String stringToSend = "[" + baseClient.getUsername() + " - " + baseClient.getIpAddress().toString() + " : "
+                    + baseClient.getPort() + "-" + formattedDate + "]: " + message;
+            out.writeUTF(stringToSend);
             out.flush(); // sends data
         } catch (IOException e) {
-            //throw new RuntimeException(e);
+           System.out.println("Message failed to send");
+            System.out.println(e);
+
         }
     }
 }
