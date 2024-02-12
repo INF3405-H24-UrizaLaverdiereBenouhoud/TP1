760121d978b43a0f8a71a19dc9d5eba834c9488f
emilielaverdiere <emilie.laverdiere@polymtl.ca>
2024-02-12 18:04:20 -0500

    one liner del

--- Client/src/main/java/ca/qc/urizalaverdierebenouhoud/MainClient.java
+++ Client/src/main/java/ca/qc/urizalaverdierebenouhoud/MainClient.java
@@ -228,7 +228,6 @@ public class MainClient {
                 while (isRunning) {
                     DataInputStream in = new DataInputStream(client.getInputStream());
                     Byte task = in.readByte();
-                    System.out.println(in);
 
                     // String history = in.readLine();
                     switch (task) {
